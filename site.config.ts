43a48263fb56439432b5cd13d0db9b15aa364018
chritch <yezk@zju.edu.cn>
2025-06-14 10:14:03 +0800

    chore: remove default page icon from site configuration

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: "https://images.zerokei.top/icons/blog-icon.png",
+  defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
